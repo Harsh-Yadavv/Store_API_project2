6bf34a9815641fff5ef4c4239596859e1ba8aaeb
Harsh Yadav <145007304+Harsh-Yadavv@users.noreply.github.com>
2024-03-08 16:21:13 +0200

    Add files via upload

---
+++ middlewares/not_found.js
@@ -0,0 +1,5 @@
+const notFound = (req, res) => {
+    res.status(404).send('Oops! Route does not exist')
+}
+
+module.exports = notFound
---
+++ models/product.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true, 'Name must be provided'],
+    },
+    price:{
+        type:Number,
+        required:[true, 'Product price must be provided'],
+    },
+    featured:{
+        type:Boolean,
+        default:false,
+    },
+    rating:{
+        type:Number,
+        default: 4.5,
+    },
+    createdAt:{
+        type:Date,
+        default: Date.now(),
+    },
+    company:{
+        type:String,
+        enum:{
+            values:['ikea', 'liddy', 'caressa', 'marcos'],
+            message: '{Value} is not supported',
+        },
+        //enum:['ikea', 'liddy', 'caressa', 'marcos'],
+    },
+})
+
+module.exports = mongoose.model('Product', productSchema)
